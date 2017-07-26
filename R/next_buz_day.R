c1fb5a46f5f598a82c38d4649a64b433dc3387da
cloud_brain <cloud_brain@qq.com>
2017-07-26 17:18:33 +0800

    添加寻找最近日期函数

---
+++ R/next_buz_day.R
@@ -0,0 +1,26 @@
+#' find next business day
+#'
+#' find the next business day
+#'
+#' @param con, rdf connection
+#' @param x, integer, date list
+#' @param including, logic, if include the today
+#'
+#' @examples
+#' x <- rep(20170101+ 1:2, 5)
+#' next_buz_day(x)
+#'
+#' @export
+#'
+next_buz_day <- function(con, x, including = F)
+{
+  buz_day <- get_buz_day(con, min(x), max(x) + 10000)
+  temp <- unique(x)
+  if(including)
+  {
+    output <- mapply(function(temp) min(buz_day[buz_day >= temp]), temp)
+  }else{
+    output <- mapply(function(temp) min(buz_day[buz_day > temp]), temp)
+  }
+  output[match(x, temp)]
+}
